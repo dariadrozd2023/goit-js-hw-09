ee7a4a041d5fdfa83f38a12cc4f5b8448a3e5018
daria drozd <daria.drozd1990@gmail.com>
2024-04-14 11:33:16 +0300

    update form 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,38 +1,34 @@
-const forms = document.querySelectorAll('.feedback-form label');
-const formInputs = [];
-
-forms.forEach(form => {
-  const input = form.querySelector('input, textarea');
-  if (input) {
-    input.addEventListener('input', saveInputToLocalStorage);
-    formInputs.push(input);
-  }
-  form.addEventListener('submit', event => {
-    event.preventDefault();
-    let isFormValid = true;
-
-    formInputs.forEach(input => {
-      if (input.value.trim() === '') {
-        isFormValid = false;
-      }
-    });
-
-    if (!isFormValid) {
-      alert('Всі поля форми повинні бути заповнені');
-      return;
-    }
-    localStorage.removeItem(feedbackFormState);
-    form.reset();
-  });
-});
-
-function saveInputToLocalStorage(event) {
+const form = document.querySelector('.feedback-form');
+// console.log(form);
+form.addEventListener('input', safeInputForm);
+function safeInputForm(event) {
   const feedbackFormState = {
-    email: document.querySelector('[name="email"]').value,
-    message: document.querySelector('[name="message"]').value,
+    email: form.querySelector('[name="email"]').value,
+    message: form.querySelector('[name="message"]').value,
   };
+
   localStorage.setItem(
     'feedback-form-state',
     JSON.stringify(feedbackFormState)
   );
+}
+form.addEventListener('submit', cleanInputForm);
+function cleanInputForm(event) {
+  event.preventDefault();
+  let isFormValid = true;
+
+  const formInputs = form.querySelectorAll('input, textarea');
+  formInputs.forEach(input => {
+    if (input.value.trim() === '') {
+      isFormValid = false;
+    }
+  });
+
+  if (isFormValid === false) {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
 }
