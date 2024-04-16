6ef953c76b6f481a5c83aad3f898c17800971fdc
daria drozd <daria.drozd1990@gmail.com>
2024-04-16 13:25:27 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,5 +1,16 @@
 const form = document.querySelector('.feedback-form');
-// console.log(form);
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+const localStorageKey = 'feedback-form-state';
+
+const savedDataForm = JSON.parse(localStorage.getItem(localStorageKey));
+
+if (savedDataForm) {
+  emailInput.value = savedDataForm.email;
+  messageInput.value = savedDataForm.message;
+}
+
 form.addEventListener('input', safeInputForm);
 function safeInputForm(event) {
   const feedbackFormState = {
